5703db16ad58e2f6b0bfa5b726b4ae55ffc98c3a
tarungajamoni <saitarun.goud1998@gmail.com>
2024-03-26 23:05:23 +0530

    Initial

---
+++ index.js
@@ -0,0 +1,46 @@
+const nm = require("nodemailer");
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+require('dotenv').config();
+
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.resolve(__dirname,'build')))
+
+app.post("/api/contact", async (req, res) => {
+    console.log("req.body", req.body)
+  const { name, email, message } = req.body;
+  const password = process.env.EMAIL_PASSWORD;
+  const transporter = nm.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: "test.personal0123@gmail.com",
+      pass: password,
+    },
+  });
+  const options = {
+    from: email,
+    to: "test.personal0123@gmail.com",
+    subject: "test subject",
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
+
+  transporter.sendMail(options, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("sent");
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
