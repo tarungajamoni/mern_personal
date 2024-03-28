b63cfe8b913e90737676ebb364b11949eb43717c
tarungajamoni <saitarun.goud1998@gmail.com>
2024-03-28 20:46:37 +0530

    final commit

--- index.js
+++ index.js
@@ -129,34 +129,76 @@
 //   console.log("Server listening on port 3000");
 // });
 
+// const nm = require("nodemailer");
+// const express = require("express");
+// const path = require("path");
+// require('dotenv').config();
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(express.static(path.resolve(__dirname,'build')));
+
+// app.post("/api", async (req, res) => {
+//     console.log("req.body", req.body)
+//   const { name, email, message } = req.body;
+//   const password = process.env.EMAIL_PASSWORD;
+//   const transporter = nm.createTransport({
+//     host: "smtp.gmail.com",
+//     port: 587,
+//     secure: false,
+//     auth: {
+//       user: "test.personal0123@gmail.com",
+//       pass: password,
+//     },
+//   });
+//   const options = {
+//     from: email,
+//     to: "test.personal0123@gmail.com",
+//     subject: "test subject",
+//     text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+//   };
+
+//     transporter.sendMail(options, function (error, info) {
+//         if (error) {
+//             console.log(error);
+//             res.status(500).send("Failed to send email.");
+//         } else {
+//             console.log("Email sent successfully");
+//             res.status(200).send("Email sent successfully");
+//         }
+//     });
+// });
+
+// app.listen(3000, () => {
+//   console.log("Server listening on port 3000");
+// });
+
 const nm = require("nodemailer");
 const express = require("express");
-const cors = require("cors");
 const path = require("path");
 require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
-//app.use(cors());
 app.use(express.static(path.resolve(__dirname,'build')));
 
 app.post("/api", async (req, res) => {
     console.log("req.body", req.body)
   const { name, email, message } = req.body;
-  const password = process.env.EMAIL_PASSWORD;
-  const transporter = nm.createTransport({
+    const transporter = nm.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
     auth: {
-      user: "test.personal0123@gmail.com",
-      pass: password,
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
     },
   });
   const options = {
     from: email,
-    to: "test.personal0123@gmail.com",
+    to: process.env.EMAIL_USER,
     subject: "test subject",
     text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
   };
@@ -171,11 +213,7 @@ app.post("/api", async (req, res) => {
         }
     });
 });
-//  module.exports = app;
-app.get('/api',(req, res)=>{
-  res.json({status:'success'})
-})
 
-app.listen('/', () => {
+app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
